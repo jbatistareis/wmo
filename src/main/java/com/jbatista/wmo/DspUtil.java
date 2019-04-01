381c96d6c0d2fcd6dec0ee9ad3233f7be92da3da
joao <j.batistareis@gmail.com>
2019-04-01 07:18:21 -0300

    separated math and dsp utilitarian methods

---
+++ src/main/java/com/jbatista/wmo/DspUtil.java
@@ -0,0 +1,47 @@
+package com.jbatista.wmo;
+
+public class DspUtil {
+
+
+    public static double oscillator(WaveForm waveForm, double amplitude, double sampleRate, double frequency, double phase, double modulation, long time) {
+        switch (waveForm) {
+            case SAWTOOTH:
+                return oscillator(waveForm, amplitude, sampleRate / frequency, phase, modulation, time);
+            default:
+                return oscillator(waveForm, amplitude, frequency / sampleRate, phase, modulation, time);
+        }
+    }
+
+    public static double oscillator(WaveForm waveForm, double amplitude, double frequency, double phase, double modulation, long time) {
+        switch (waveForm) {
+            case SINE:
+                return amplitude * sineWave(frequency, phase, modulation, time);
+            case SQUARE:
+                return amplitude * squareWave(frequency, phase, modulation, time);
+            case TRIANGLE:
+                return amplitude * triangleWave(frequency, phase, modulation, time);
+            case SAWTOOTH:
+                return -(2 * amplitude / Math.PI) * sawtoothWave(frequency, phase, modulation, time);
+            default:
+                throw new AssertionError(waveForm.name());
+        }
+    }
+
+    private static double sineWave(double frequency, double phase, double modulation, long time) {
+        return Math.sin((2 * Math.PI * frequency * time + phase) + modulation);
+    }
+
+    private static double squareWave(double frequency, double phase, double modulation, long time) {
+        return Math.signum(sineWave(frequency, phase, modulation, time));
+    }
+
+    private static double triangleWave(double frequency, double phase, double modulation, long time) {
+        return 2 / Math.PI * Math.asin(sineWave(frequency, phase, modulation, time));
+    }
+
+    // TODO modulation
+    private static double sawtoothWave(double frequency, double phase, double modulation, long time) {
+        return Math.atan(1.0 / Math.tan((time * Math.PI / frequency) + phase));
+    }
+
+}
--- src/main/java/com/jbatista/wmo/MathUtil.java
+++ src/main/java/com/jbatista/wmo/MathUtil.java
@@ -2,53 +2,24 @@ package com.jbatista.wmo;
 
 public class MathUtil {
 
+    public static int valueIn16bit(byte b1, byte b2) {
+        return ((b1 & 0xFF) << 8) + (b2 & 0xFF);
+    }
+
+    public static int valueIn24bit(byte b1, byte b2, byte b3) {
+        return ((b1 & 0xFF) << 16) + ((b2 & 0xFF) << 8) + (b3 & 0xFF);
+    }
+
+    public static int valueIn32bit(byte b1, byte b2, byte b3, byte b4) {
+        return ((b1 & 0xFF) << 24) + ((b2 & 0xFF) << 16) + ((b3 & 0xFF) << 8) + (b4 & 0xFF);
+    }
+
     public static double lerp(double start, double end, double factor) {
         return start + factor * (end - start);
     }
 
     public static double frequenceByPosition(int position) {
         return 69.297 * Math.pow(2, (position - 49) / 12d);
-    }
-
-    public static double oscillator(WaveForm waveForm, double amplitude, double sampleRate, double frequency, double phase, double modulation, long time) {
-        switch (waveForm) {
-            case SAWTOOTH:
-                return oscillator(waveForm, amplitude, sampleRate / frequency, phase, modulation, time);
-            default:
-                return oscillator(waveForm, amplitude, frequency / sampleRate, phase, modulation, time);
-        }
-    }
-
-    public static double oscillator(WaveForm waveForm, double amplitude, double frequency, double phase, double modulation, long time) {
-        switch (waveForm) {
-            case SINE:
-                return amplitude * sineWave(frequency, phase, modulation, time);
-            case SQUARE:
-                return amplitude * squareWave(frequency, phase, modulation, time);
-            case TRIANGLE:
-                return amplitude * triangleWave(frequency, phase, modulation, time);
-            case SAWTOOTH:
-                return -(2 * amplitude / Math.PI) * sawtoothWave(frequency, phase, modulation, time);
-            default:
-                throw new AssertionError(waveForm.name());
-        }
-    }
-
-    private static double sineWave(double frequency, double phase, double modulation, long time) {
-        return Math.sin((2 * Math.PI * frequency * time + phase) + modulation);
-    }
-
-    private static double squareWave(double frequency, double phase, double modulation, long time) {
-        return Math.signum(sineWave(frequency, phase, modulation, time));
-    }
-
-    private static double triangleWave(double frequency, double phase, double modulation, long time) {
-        return 2 / Math.PI * Math.asin(sineWave(frequency, phase, modulation, time));
-    }
-
-    // TODO modulation
-    private static double sawtoothWave(double frequency, double phase, double modulation, long time) {
-        return Math.atan(1.0 / Math.tan((time * Math.PI / frequency) + phase));
     }
 
 }
