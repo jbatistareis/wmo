13bffc1c56f2c45f3533f1cb7b84316a8b1ec08d
j.batistareis <j.batistareis@gmail.com>
2019-10-15 01:20:04 -0300

    method call bugfixes, working sawtooth

--- src/main/java/com/jbatista/wmo/DspUtil.java
+++ src/main/java/com/jbatista/wmo/DspUtil.java
@@ -10,30 +10,27 @@ public class DspUtil {
                 return squareWave(sampleRate, frequency, phase, modulation, time);
             case TRIANGLE:
                 return triangleWave(sampleRate, frequency, phase, modulation, time);
-            // TODO
             case SAWTOOTH:
-                return 0;
-            // return -(2 * amplitude / Math.PI) * sawtoothWave(frequency, phase, modulation, time);
+                return sawtoothWave(sampleRate, frequency, phase, modulation, time);
             default:
                 throw new AssertionError(waveForm.name());
         }
     }
 
     private static double sineWave(double sampleRate, double frequency, double phase, double modulation, long time) {
-        return Math.sin((MathUtil.PI_T2 * frequency * time) / sampleRate + phase) + modulation;
+        return Math.sin((MathUtil.TAU * frequency * time) / sampleRate + phase) + modulation;
     }
 
     private static double squareWave(double sampleRate, double frequency, double phase, double modulation, long time) {
-        return Math.signum(sineWave(frequency, sampleRate, phase, modulation, time));
+        return Math.signum(sineWave(sampleRate, frequency, phase, modulation, time));
     }
 
     private static double triangleWave(double sampleRate, double frequency, double phase, double modulation, long time) {
-        return MathUtil.PI_D2 * Math.asin(sineWave(frequency, sampleRate, phase, modulation, time));
+        return MathUtil.PI_D2 * Math.asin(sineWave(sampleRate, frequency, phase, modulation, time));
     }
 
-    // TODO
-    private static double sawtoothWave(double frequency, double sampleRate, double phase, double modulation, long time) {
-        return Math.atan(1.0 / Math.tan((time * Math.PI / frequency) + phase));
+    private static double sawtoothWave(double sampleRate, double frequency, double phase, double modulation, long time) {
+        return -MathUtil.PI_D2 * Math.atan(1 / Math.tan(((Math.PI * time * frequency) / sampleRate)) + phase);
     }
 
 }
